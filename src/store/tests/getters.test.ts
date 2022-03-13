f28695ec1af37d4f90570facc6ab6ba12b3f2465
zhenyuWang <13641039885@163.com>
2022-03-13 14:23:48 +0800

    refactor: remove unuse test

--- src/store/tests/getters.test.ts
+++
@@ -1,6 +0,0 @@
-import { useUserStore } from "@/store/user";
-test("userInfo", () => {
-  const userInfo = useUserStore().userInfo;
-  expect(userInfo).not.toBeNull();
-  expect(userInfo).not.toBeUndefined();
-});
