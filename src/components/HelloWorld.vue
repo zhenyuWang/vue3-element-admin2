abef64ac4497e67045a144d36fcfc8b582c49d01
zhenyuWang <13641039885@163.com>
2022-04-06 23:59:44 +0800

    refactor: remove unuse calss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,11 @@
 <template>
   <h1 class="c-3477F2">{{ msg }}</h1>
   <p>{{ count }}</p>
-  <el-button class="plus" type="primary" @click="count++"> plus </el-button>
+  <el-button type="primary" @click="count++"> plus </el-button>
 </template>
 
 <script setup lang="ts">
 import { defineProps, ref } from "vue"
-// 获取 props
 defineProps({
   msg: {
     type: String,
